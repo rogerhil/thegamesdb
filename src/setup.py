ec0b083e55497948f0893b5a111a24cffa0f1287
Rogerio Hilbert Lima <rogerhil@gmail.com>
2015-09-30 22:01:55 +0100

    Continuous integration files

---
+++ src/setup.py
@@ -0,0 +1,16 @@
+import os
+from setuptools import setup, find_packages
+
+local_file = lambda f: open(os.path.join(os.path.dirname(__file__), f)).read()
+
+if __name__ == '__main__':
+    setup(
+        name='thegamesdb',
+        version='0.1',
+        description='The Games DB API wrapper for Python',
+        long_description=local_file('README.md'),
+        author='Rogerio Hilbert Lima',
+        author_email='rogerhil@gmail.com',
+        url='https://github.com/rogerhil/thegamesdb',
+        packages=find_packages()
+    )
