5fd572eca713f23f6ed7883b5bb5262f1dc708b4
Rogerio Hilbert Lima <rogerio.hilbert.lima@ericsson.com>
2016-01-14 11:25:19 +0000

    Preparing to register at PyPI

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 import os
 from setuptools import setup, find_packages
 
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 local_file = lambda f: open(os.path.join(os.path.dirname(__file__), f)).read()
 
 if __name__ == '__main__':
@@ -8,7 +14,7 @@ if __name__ == '__main__':
         name='thegamesdb',
         version='0.2',
         description='The Games DB API wrapper for Python',
-        long_description=local_file('README.md'),
+        long_description=long_description,
         author='Rogerio Hilbert Lima',
         author_email='rogerhil@gmail.com',
         url='https://github.com/rogerhil/thegamesdb',
