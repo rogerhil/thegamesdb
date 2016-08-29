b64f039f86000ae1b9c512ce1d902db1840cf050
Rogerio Hilbert Lima <rogerhil@gmail.com>
2016-08-29 23:52:04 +0100

    Updates api support.

--- thegamesdb/api.py
+++ thegamesdb/api.py
@@ -22,9 +22,6 @@ Games DB API (http://wiki.thegamesdb.net/index.php/Main_Page).
 __author__ = "Rogerio Hilbert Lima"
 __copyright__ = "Copyright (C) 2015 Rogerio Hilbert Lima <rogerhil@gmail.com>"
 __license__ = "GPL"
-__version__ = "0.1"
-__maintainer__ = "Rogerio Hilbert Lima"
-__email__ = "rogerhil@gmail.com"
 
 import time
 from urllib.parse import urlencode
